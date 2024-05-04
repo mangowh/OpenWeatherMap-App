125fa4d0ead59ddf61ef8b3021d06ccc1e370973
s.tambellini <s.tambellini@icloud.com>
2024-05-04 13:20:01 +0200

    serviceworker

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,19 +1,20 @@
-import {
-  provideHttpClient,
-  withInterceptors
-} from "@angular/common/http";
-import { ApplicationConfig } from "@angular/core";
+import { provideHttpClient } from "@angular/common/http";
+import { ApplicationConfig, isDevMode } from "@angular/core";
 import { provideRouter } from "@angular/router";
+import { provideServiceWorker } from "@angular/service-worker";
 import { provideNgIconsConfig } from "@ng-icons/core";
 import { routes } from "./app.routes";
-import { cacheInterceptor } from "./interceptors/cache.interceptor";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(withInterceptors([/* cacheInterceptor */])),
+    provideHttpClient(),
     provideNgIconsConfig({
       size: "1.5em",
+    }),
+    provideServiceWorker("ngsw-worker.js", {
+      // enabled: !isDevMode(),
+      registrationStrategy: "registerImmediately",
     }),
   ],
 };
